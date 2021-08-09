bf78bfdc8aa0b1c9a8fa25ff5c566da2879ac396
Cameron <camcreel@gmail.com>
2021-08-09 12:33:02 -0400

    Add paths in application.rb

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,120 @@ class Application
     res = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+    content_type = { 'Content-Type' => 'application/json' }
+
+    if req.path == "/movies" && req.get?
+      movies = Movie.all
+      return [
+        200, 
+        content_type, 
+        [ movies.to_json ]
+      ]
+
+    elsif req.path.match('/movies/') && req.post?
+      body = JSON.parse(req.body.read)
+      movie = Movie.create(body)
+      return [
+        200, 
+        content_type, 
+        [ movie.to_json ]
+      ]
+
+    elsif req.path.match('/movies/') && req.get?
+      id = req.path.split('/')[2]
+      begin
+        movie = Movie.find(id)
+        return [200, content_type, [movie.as_json(include: :movie_reviews).to_json] ]
+      rescue
+        return [404, content_type, [{message: "Movie not found"}.to_json]]
+      end
+    
+
+    elsif req.path == '/reviewers' && req.get?
+      reviewers = Reviewer.all
+      return [
+        200,
+        content_type,
+        [reviewers.to_json]
+      ]
+
+    elsif req.path.match('/reviewers') && req.post?
+      body = JSON.parse(req.body.read)
+      reviewer = Reviewer.create(body)
+      return [
+        200, 
+        content_type, 
+        [reviewer.to_json ]
+      ]
+    elsif req.path.match('/reviewers/') && req.get?
+      id = req.path.split('/')[2]
+      begin
+        reviewer = Reviewer.find(id)
+        return [200, content_type, [reviewer.as_json(include: :movie_reviews).to_json] ]
+      rescue
+        return [404, content_type, [{message: "Reviewer not found"}.to_json]]
+      end
+    
+
+
+    elsif req.path == '/movie_reviews' && req.get?
+      movie_reviews = MovieReview.all
+      return [200, content_type, [movie_reviews.to_json]]
+
+    elsif req.path.match('/movie_reviews') && req.post?
+      body = JSON.parse(req.body.read)
+      movie_review = MovieReview.create(body)
+      return [
+        200, 
+        content_type, 
+        [ movie_review.to_json ]
+      ]
+
+    elsif req.path.match('/movie_reviews/') && req.get?
+      id = req.path.split('/')[2]
+      begin
+        movie_review = MovieReview.find(id)
+        return [200, content_type, [movie_review.as_json(include: [:movie,:reviewer]).to_json] ]
+      rescue
+        return [404, content_type, [{message: "Movie Review not found"}.to_json]]
+      end
+    
+    elsif req.path.match('/movie_reviews/') && req.patch?
+      id = req.path.split('/')[2]
+      body = JSON.parse(req.body.read)
+      begin
+        movie_review = MovieReview.find(id)
+        movie_review.update(body)
+      rescue
+        return [404, content_type, [{message: "Movie Review not found"}.to_json]]
+      end
+
+    
+    elsif req.path.match('/movie_reviews/') && req.delete?
+      id = req.path.split('/')[2]
+      begin
+        movie_review = MovieReview.find(id)
+        movie_review.destroy
+        return [200, content_type, [{message: "Movie Review Destroyed"}.to_json] ]
+      rescue
+        return [404, content_type, [{message: "Movie Review not found"}.to_json]]
+      end
+    
+    # elsif req.path.match('/movies?name=') && req.get?
+    #   query = req.path.split('name=')[1]
+    #   movie = Movie.find_by(name: query)
+    #   if movie
+    #     return  [
+    #       200, 
+    #       content_type, 
+    #       [ movie.to_json ]
+    #     ]
+    #   else
+    #     return [404, content_type, [{message: "Movie not found"}.to_json]]
+    #   end
 
     else
-      res.write "Path Not Found"
+      return [404, content_type, [ {:message => "Path not Found"}.to_json ]]
 
     end
 
